6ea4f993399d00257e17c20ae230bd43507abaea
Cecilia Yu <ceciliayu@Ceciliade-MacBook-Pro.local>
2018-03-12 17:08:16 +0800

    Add public.

--- server.js
+++ server.js
@@ -2,13 +2,15 @@ var express = require("express");
 var app     = express();
 var path    = require("path");
 
+app.use(express.static(path.join(__dirname,'public')));
+
 app.get('/',function(req,res){
   res.sendFile(path.join(__dirname+'/index.html'));
   //__dirname : It will resolve to your project folder.
 });
 
-app.get('/flowplayer',function(req,res){
-  res.sendFile(path.join(__dirname+'/flowplayer/flowplayer-rtmp.html'));
+app.get('/abc',function(req,res){
+  res.sendFile(path.join(__dirname+'/public/flowplayer/flowplayer-rtmp.html'));
 });
 
 app.listen(8888);
